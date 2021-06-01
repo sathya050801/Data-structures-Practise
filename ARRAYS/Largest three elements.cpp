f1b642dc498c57e8b265578f699bf46a73c89126
T Sathya Narayana <61154612+sathya050801@users.noreply.github.com>
2021-06-01 21:16:42 +0530

    Add files via upload

---
+++ ARRAYS/Largest three elements.cpp
@@ -0,0 +1,41 @@
+// C++ program to print common elements in three arrays
+#include <bits/stdc++.h>
+using namespace std;
+	
+void threeLargest(int arr[],int n)
+{
+int first,second,third;
+first=second=third=arr[0];
+for(int i=0;i<n;i++)
+{
+    if(arr[i]>first)
+    {
+    	third=second;
+    	second=first;
+    	first=arr[i];
+	}
+	
+	else if(arr[i]>second)
+	{
+		third=second;
+		second=arr[i];
+	}
+	
+	else
+	{
+		third=arr[i];
+	}
+}
+	
+	cout<<"The three largest elements are "<<first<<" "<<second<<" "<<third;
+}
+
+ 
+// Driver code
+int main()
+{
+  int arr[]={1,2,3,4,5};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  threeLargest(arr,n);
+      return 0;
+}
